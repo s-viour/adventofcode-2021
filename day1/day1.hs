25b69b0c1e1f8fbf74732e17738e2de844e2b262
Matt Wyatt <mwyatt1000@gmail.com>
2021-12-02 03:41:01 -0500

    update day1 solution with both parts and full exe

--- day1/day1.hs
+++ day1/day1.hs
@@ -1,5 +1,8 @@
 module Main where
 
+import System.Environment ( getArgs ) 
+import System.IO ( stderr, hPutStrLn )
+
 
 getValues :: IO [Int]
 getValues = map (read :: String -> Int) . lines <$> getContents
@@ -19,8 +22,25 @@ createPairs values = zip values (tail values)
 countIncreasing :: [(Int, Int)] -> Int
 countIncreasing = length . filter (uncurry (<))
 
-solve :: [Int] -> Int
-solve = countIncreasing . createPairs . createSumList . createWindows
+solve1 :: [Int] -> Int
+solve1 = countIncreasing . createPairs
+
+solve2 :: [Int] -> Int
+solve2 = countIncreasing . createPairs . createSumList . createWindows
+
+getSolver :: [String] -> Maybe ([Int] -> Int)
+getSolver [] = Nothing
+getSolver args
+  | head args == "solve1" = Just solve1
+  | head args == "solve2" = Just solve2
+  | otherwise = Nothing
 
 main :: IO ()
-main = getValues >>= (print . solve)
+main = do
+  values <- getValues
+  args <- getArgs
+  case getSolver args of
+    Nothing -> usage
+    Just s -> (print .s) values
+  where
+    usage = hPutStrLn stderr "usage: day1 [solve1|solve2]"
