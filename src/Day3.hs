eca9c1125a4b849a66aaa72b88233e03907e3a1f
Matt Wyatt <mwyatt1000@gmail.com>
2021-12-09 23:30:04 -0500

    add day 3 part 2 solution!

--- src/Day3.hs
+++ src/Day3.hs
@@ -1,7 +1,7 @@
-
-
-module Day3 where
-
+module Day3
+  ( dayMain1
+  , dayMain2
+  ) where
 
 import System.IO ( hPutStrLn, stderr )
 import Data.Bits ( testBit, shiftR, complement )
@@ -21,7 +21,7 @@ highestSetBit b = (length . takeWhile (/= 0)) (iterate (`shiftR` 1) b) - 1
 
 mostCommonBit :: Int -> [Int] -> Int
 mostCommonBit pos nums =
-  if ones > zeros then 1
+  if ones >= zeros then 1
   else 0
   where
     extracted = map (`testBit` pos) nums
@@ -34,6 +34,21 @@ leastCommonBit pos nums = if mostCommonBit pos nums == 1 then 0 else 1
 iterateBits :: (Int -> [Int] -> Int) -> [Int] -> Int -> [Int] -> [Int]
 iterateBits f bits 0 numbers = bits ++ [f 0 numbers]
 iterateBits f bits n numbers = bits ++ [f n numbers] ++ iterateBits f bits (n - 1) numbers
+
+co2OxRate :: (Int -> [Int] -> Int) -> [Int] -> Int -> [Int]
+co2OxRate _ [x] _ = [x]
+co2OxRate bitFxn nums pos = co2OxRate bitFxn (filter fxn nums) (pos - 1)
+  where
+    mc = bitFxn pos nums
+    fxn = 
+      if mc == 1 then (`testBit` pos)
+      else (\n -> not $ n `testBit` pos)
+
+oxygenRate :: [Int] -> Int -> [Int]
+oxygenRate = co2OxRate mostCommonBit
+
+co2Rate :: [Int] -> Int -> [Int]
+co2Rate = co2OxRate leastCommonBit
 
 gammaRate :: [Int] -> Int -> [Int] -> [Int]
 gammaRate = iterateBits mostCommonBit
@@ -51,4 +66,10 @@ dayMain1 = do
   print (gr * er)
 
 dayMain2 :: IO ()
-dayMain2 = hPutStrLn stderr "this part is unimplemented!"
+dayMain2 = do
+  contents <- getContents
+  let numbers = getNumbers contents
+  let bitPos = maximum (map highestSetBit numbers)
+  let [o2] = oxygenRate numbers bitPos
+  let [co2] = co2Rate numbers bitPos
+  print (o2 * co2)
